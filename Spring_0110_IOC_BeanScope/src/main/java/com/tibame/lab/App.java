a6536f8ed0e63280b5e01aedf9e1b7abb7a2cfe3
Chen, Sander <Sander.Chen@commscope.com>
2023-10-02 10:26:20 +0800

    :beer: Add Spring 0110 lab

--- Spring_0110_IOC_BeanScope/src/main/java/com/tibame/lab/App.java
+++ Spring_0110_IOC_BeanScope/src/main/java/com/tibame/lab/App.java
@@ -1,5 +1,44 @@
 package com.tibame.lab;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.tibame.lab.beans.BMWCar;
+import com.tibame.lab.beans.BenzCar;
+import com.tibame.tutorial.beans.BeanA;
+
 public class App {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-lab.xml");
+		
+		var benzCar1 =  context.getBean(BenzCar.class);
+		
+		var benzCar2 =  context.getBean(BenzCar.class);
+		
+		System.out.println("Benz1:" + benzCar1.hashCode());
+		System.out.println("Benz2:" + benzCar2.hashCode());
+		
+		if (benzCar1 != benzCar2) {
+			System.out.println("GO FIX YOUR XML!");
+			return;
+		}
+		
+		var bmwCar1 =  context.getBean(BMWCar.class);
+		
+		var bmwCar2 =  context.getBean(BMWCar.class);
+		
+		System.out.println("BMW1:" + bmwCar1.hashCode());
+		System.out.println("BMW2:" + bmwCar2.hashCode());
+
+		if (bmwCar1 == bmwCar2) {
+			System.out.println("GO FIX YOUR XML!");
+			return;
+		}
+		
+		System.out.println("You make it!");
+
+	}
 
 }
---
+++ Spring_0110_IOC_BeanScope/src/main/java/com/tibame/lab/beans/BMWCar.java
@@ -0,0 +1,5 @@
+package com.tibame.lab.beans;
+
+public class BMWCar {
+
+}

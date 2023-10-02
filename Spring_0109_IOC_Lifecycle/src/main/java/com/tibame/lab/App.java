913e362f3d98f7ac693cb06ba4ab894fe1270b9f
Chen, Sander <Sander.Chen@commscope.com>
2023-10-02 10:18:27 +0800

    add Spring 0109 lab

--- Spring_0109_IOC_Lifecycle/src/main/java/com/tibame/lab/App.java
+++ Spring_0109_IOC_Lifecycle/src/main/java/com/tibame/lab/App.java
@@ -1,5 +1,36 @@
 package com.tibame.lab;
 
+import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.tibame.lab.beans.BenzCar;
+import com.tibame.lab.beans.MessageReceiver;
+import com.tibame.lab.beans.tire.BMWTire;
+import com.tibame.lab.beans.tire.BenzTire;
+
 public class App {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-lab.xml");
+		
+		var benzcar = context.getBean(BenzCar.class);
+		if (!(benzcar.getTireA() instanceof BenzTire)) {
+			System.out.println("GO FIX YOUR XML!");
+			return;
+		}
+		
+		if (!(benzcar.getTireB() instanceof BMWTire)) {
+			System.out.println("GO FIX YOUR XML!");
+			return;
+		}
+		
+	    DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) context.getAutowireCapableBeanFactory();
+	    registry.destroySingleton("benzCar");
+	    
+	    MessageReceiver.validate();
+		
+	}
 
 }
---
+++ Spring_0109_IOC_Lifecycle/src/main/java/com/tibame/lab/beans/BenzCar.java
@@ -0,0 +1,46 @@
+package com.tibame.lab.beans;
+
+import com.tibame.lab.beans.tire.BMWTire;
+import com.tibame.lab.beans.tire.BenzTire;
+import com.tibame.lab.beans.tire.Tire;
+
+public class BenzCar {
+	
+	private BenzTire tireA;
+	private BMWTire tireB;
+
+	
+	
+	public BenzCar(BenzTire tireA, BMWTire tireB) {
+		System.out.println("This is constructor!");
+		this.tireA = tireA;
+		this.tireB = tireB;
+	}
+
+	public void lalalaThisIsInit() {
+		System.out.println("This is Post Constructor method, Init complete");
+		MessageReceiver.sendMessage("Init!");
+	}
+	
+	public void lalalaThisIsDestroy() {
+		System.out.println("This is Pre-destroy method, Ready for destroy");
+		MessageReceiver.sendMessage("Destroy!");
+	}
+
+	public BenzTire getTireA() {
+		return tireA;
+	}
+
+	public void setTireA(BenzTire tireA) {
+		this.tireA = tireA;
+	}
+
+	public BMWTire getTireB() {
+		return tireB;
+	}
+
+	public void setTireB(BMWTire tireB) {
+		this.tireB = tireB;
+	}
+
+}

c57042598d12d022ec6ba2d31adadd85b0720ad1
DESKTOP-U2BJ55P\Sander <sanderxavalon@gmail.com>
2023-10-06 07:09:02 +0800

    minor fix

---
+++ Spring_0116_IOC_ServletIntegrate_XML/src/main/java/com/tibame/tutorial/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.tibame.tutorial;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.tibame.tutorial.beans.Car;
+
+public class HelloWorld extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+		Car car = ctx.getBean(Car.class);
+		System.out.println(car.toString());
+		PrintWriter out = response.getWriter();
+		out.write(car.toString());
+	}
+}
